19d4a887ca67bd7cdeaeb8a4c043de9e0b4701ac
jocopa3 <jocopa3@gmail.com>
2016-05-14 00:31:01 -0500

    Fixed issue with modloader not working on non-English systems

--- Zenova/ZenovaModLoader/ZenovaModLoader/ModLoader.cpp
+++ Zenova/ZenovaModLoader/ZenovaModLoader/ModLoader.cpp
@@ -12,19 +12,24 @@ DWORD ModLoader::AdjustGroupPolicy(std::wstring wstrFilePath)
 	EXPLICIT_ACCESS eaAccess;
 	SECURITY_INFORMATION siInfo = DACL_SECURITY_INFORMATION;
 	DWORD dwResult = ERROR_SUCCESS;
+	PSID pSID;
 
 	// Get a pointer to the existing DACL (Conditionaly).
 	dwResult = GetNamedSecurityInfo(wstrFilePath.c_str(), SE_FILE_OBJECT, DACL_SECURITY_INFORMATION, NULL, NULL, &pOldDACL, NULL, &pSD);
 	if (dwResult != ERROR_SUCCESS)
 		goto Cleanup;
 
+	ConvertStringSidToSid(L"S-1-15-2-1", &pSID);
+	if (pSID == NULL)
+		goto Cleanup;
+
 	ZeroMemory(&eaAccess, sizeof(EXPLICIT_ACCESS));
 	eaAccess.grfAccessPermissions = GENERIC_READ | GENERIC_EXECUTE;
 	eaAccess.grfAccessMode = SET_ACCESS;
 	eaAccess.grfInheritance = SUB_CONTAINERS_AND_OBJECTS_INHERIT;
-	eaAccess.Trustee.TrusteeForm = TRUSTEE_IS_NAME;
+	eaAccess.Trustee.TrusteeForm = TRUSTEE_IS_SID;
 	eaAccess.Trustee.TrusteeType = TRUSTEE_IS_WELL_KNOWN_GROUP;
-	eaAccess.Trustee.ptstrName = L"ALL APPLICATION PACKAGES";
+	eaAccess.Trustee.ptstrName = (LPWSTR)pSID;
 
 	// Create a new ACL that merges the new ACE into the existing DACL.
 	dwResult = SetEntriesInAcl(1, &eaAccess, pOldDACL, &pNewDACL);
